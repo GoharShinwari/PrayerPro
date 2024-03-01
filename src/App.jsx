c53ff98f3bc1bed71dd850ec0940c3d7ba02881d
GoharShinwari <121142075+GoharShinwari@users.noreply.github.com>
2024-03-01 16:23:38 -0500

    little fixes + future changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import DigitalClock from './DigitalClock.jsx';
-import PrayerTimes from './PrayerTimes.jsx';
-import RamadanCounter from './RamadanCounter.jsx';
+import DigitalClock from './components/DigitalClock.jsx';
+import PrayerTimes from './components/PrayerTimes.jsx';
+import RamadanCounter from './components/RamadanCounter.jsx';
 
 function App() {
   return (
-    <div>
-      <DigitalClock />
-      <PrayerTimes />
-      <RamadanCounter />
-    </div>
+     <div>
+        <DigitalClock />
+        <PrayerTimes />
+        <RamadanCounter />
+      </div>
   );
 }
 
 export default App;
+
+//    How to make a top bar
+ // <TopBar />
+ // <div style={{ paddingTop: 50 }}
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,22 @@
+
+
+/* import React from 'react';
+
+function TopBar() {
+  // Handle button click to navigate to a link
+  const handleButtonClick = () => {
+    // Replace '/your-link' with the actual link you want to navigate to
+    window.location.href = '/your-link';
+  };
+
+  return (
+    <div className="top-bar">
+      <img src="./src/assets/masjid.svg" alt="Logo" className="logo" />
+
+      <button className="button" onClick={handleButtonClick}>Islamic Resources</button>
+    </div>
+  );
+}
+
+export default TopBar;
+*/
